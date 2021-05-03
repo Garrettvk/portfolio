dc3a489afb3ef4e0dc278411095586925319e38f
Garrett <gvk795@gmail.com>
2021-05-03 01:56:08 -0700

    html added for ProjectsPage

---
+++ src/Components/ProjectsPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <div id="about">
+      <div className="projectsPage__left">
+        <h1>Netflix Clone</h1>
+        <p>Made with react for the UI, firebase for authentication and database. Stripe for payment processing</p>
+        <div className="profjectsPage__buttons">
+          <button>Live Demo</button>
+          <button>Code On Github</button>
+        </div>
+      </div>
+      <div className="projectsPage__right">
+        <img src="https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt=""/>
+      </div>
+    </div>
+  );
+};
+
+export default About;
