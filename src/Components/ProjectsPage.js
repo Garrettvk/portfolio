3cfa93406ac3c67ef8a3cf8c2740339fa90b7b5d
Garrett <gvk795@gmail.com>
2021-05-06 05:03:40 -0700

    picture changed for netflix project

--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import './ProjectsPage.css'
 
+// project photo
+import netflixPic from './../images/netflixPic.png'
+
 const ProjectsPage = () => {
 
   return (
@@ -9,12 +12,12 @@ const ProjectsPage = () => {
         <h1>Netflix Clone</h1>
         <p>Made with react for the UI, firebase for authentication and database. Stripe for payment processing</p>
         <div className="profjectsPage__buttons">
-          <a href="https://netflix-build-76aae.web.app/" target="_blank">Live Demo</a>
-          <a href="https://github.com/Garrettvk/netflix-build" target="_blank">Code On Github</a>
+          <a href="https://netflix-build-76aae.web.app/" target="_blank" rel="noreferrer">Live Demo</a>
+          <a href="https://github.com/Garrettvk/netflix-build" target="_blank" rel="noreferrer">Code On Github</a>
         </div>
       </div>
       <div className="projectsPage__right">
-        <img src="https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg" alt="" />
+        <img src={netflixPic} alt="" />
       </div>
     </div>
   );
