e6a2d9c85f1b9be887607165c05fac9abbb1f0d3
Garrett <gvk795@gmail.com>
2021-05-05 23:24:57 -0700

    unused variables removed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,17 +5,7 @@ const Header = ({ data }) => {
   if (data) {
     var name = data.name;
     var occupation = data.occupation;
-    var description = data.description;
     var city = data.address.city;
-    var networks = data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
   }
 
   return (
