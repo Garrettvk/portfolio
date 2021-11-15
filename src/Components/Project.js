a43e2c42ef9f8f0184fd4dacbe01d545f433a788
Gary <gvk795@gmail.com>
2021-11-15 00:54:48 -0800

    css for project photos on mobile

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import './Project.css'
 
-function Project({title, description, projectUrl, githubUrl, image}) {
+function Project({title, projectUrl, image}) {
   return (
     <div id="projectsPage">
-      <div className="projectsPage__left">
         <h1>{title}</h1>
-        <span>{description}</span>
-        <div className="projectsPage__buttons">
-          <a href={projectUrl} target="_blank" rel="noreferrer">Live Demo</a>
-          {githubUrl && <a href={githubUrl} target="_blank" rel="noreferrer">Code On Github</a>}
-        </div>
-      </div>
-      <div className="projectsPage__right">
         <img src={image} alt="" />
-      </div>
     </div>
   )
 }
--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Project from "./Project";
+import './ProjectsPage.css'
 
 // project photos
 import fmGallery from './../images/FM-Gallery.png'
 import lcfm from './../images/lcfm.png'
 import greaterWorks from './../images/greaterWorks.png'
-import wolfsToolBox from './../images/wolfs-toolbox.png'
 
 const ProjectsPage = () => {
 
@@ -16,11 +16,6 @@ const ProjectsPage = () => {
       <Project
         title='FM Gallery'
         image={fmGallery}
-        description={
-          <div>
-            Landing pages for products created by RCC. These websites were designed by me using HTML and CSS. I created my own graphics with Photoshop, wrote copy for written content, utilized code from the company code base as well wrote code from scratch when needed. These pages are fully responsive and used by RCC to showcase and sell their software.
-          </div>
-        }
         projectUrl='https://fmgallery.net/'
       />
 
@@ -28,34 +23,14 @@ const ProjectsPage = () => {
       <Project
         title='LiveCode for FM'
         image={lcfm}
-        description={
-          <div>
-            Made with react for the UI, firebase for user authentication and database. Stripe for payment processing. Items can be added and removed from cart and then purchased by typing "42" until the card details are filled in.
-            <div>Logging in is optional.</div>
-            <div>You can login in with:</div>
-            <div>username: admin@gmail.com</div>
-            <div>password: 123456</div>
-          </div>
-        }
         projectUrl='https://clone-9e900.web.app/'
-        githubUrl='https://github.com/Garrettvk/amazon-clone'
-      />
-
-      {/* Wolf's Toolbox */}
-      <Project
-        title="Wolf's Toolbox"
-        image={wolfsToolBox}
-        description=''
-        projectUrl='https://fmstartingpoint.com/marketplace/wolftoolbox/'
       />
 
       {/* Greater Works */}
       <Project
         title='Greater Works Construction'
         image={greaterWorks}
-        description='Website created for local construction company. Built with mostly html and css. The slideshow and contact form were made with react. The email functionality was made with the email-js library.'
         projectUrl='http://greaterworksconstruction.com/'
-        githubUrl='https://github.com/Garrettvk/Greater-Works-Website/'
       />
     </div>
   );
