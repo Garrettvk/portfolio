d36ddbc5c7f0abebec3a93f1bbaa263741f265df
Gary <gvk795@gmail.com>
2021-11-18 21:35:30 -0800

    about me section made for mobile

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,27 +1,30 @@
 import React from "react";
-import './About.css'
+import "./About.css";
 
 const About = ({ data }) => {
     return (
-        <section id="about">
-            <div className="row">
-                <h2>About Me</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Dignissimos magni earum corporis quibusdam, facilis neque,
-                    deserunt adipisci soluta explicabo eum maxime molestiae
-                    optio iste quas, nesciunt accusamus. Fuga, cum voluptate.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Corporis porro eveniet numquam optio enim repudiandae
-                    maiores debitis aut dignissimos, mollitia perferendis illo
-                    sit. Ducimus dolorem quidem tempore fugiat ratione magni!
-                </p>
+        <div className="about">
+            <h2>About Me</h2>
+            <p>
+                Hello, my name is Garrett van Kranenburg. I am a web developer
+                from San Luis Obispo, California. I am currently working at
+                Richard Carlton Consulting in Los Osos, California. Currently I
+                am working on updating and creating new web pages for products
+                and services.
+            </p>
 
-                <a href={"https://www.youtube.com/watch?v=5qap5aO4i9A"}>
-                    <i className="fa fa-download"></i>Download Resume
-                </a>
-            </div>
-        </section>
+            <p>
+                My projects include websites that I have worked on for Richard
+                Carlton Consulting as well as a website for a local construction
+                company that was made with React. I am currently looking for
+                more opportunities in web development and would love to hear
+                from you.
+            </p>
+
+            <a href={"https://www.youtube.com/watch?v=5qap5aO4i9A"}>
+                <i className="fa fa-download"></i>Download Resume
+            </a>
+        </div>
     );
 };
 
