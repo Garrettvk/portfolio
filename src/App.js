d54fe079b76953e4bf7564ed65cf82bba14eff5a
BigPump <garrettvk.dev@gmail.com>
2022-01-15 18:55:03 -0800

    files added for beny template

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from "react";
-
-import Header from "./Components/Header";
-import ProjectsPage from "./Components/ProjectsPage";
-import About from "./Components/About"
-
-import "./App.css";
+import React, { useEffect } from "react";
+import Routes from "./router/Routes";
+import ScrollToTop from "./components/ScrollToTop";
+import AnimatedCursor from "react-animated-cursor";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
-  const [resumeData, setResumeData] = useState({});
-
   useEffect(() => {
-    fetch("/resumeData.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setResumeData(data);
-      });
+    AOS.init();
   }, []);
-
   return (
-    <div className="App">
-      <Header data={resumeData.main} />
-      <About />
-      <ProjectsPage data={resumeData.main} />
+    <div className="beny_tm_all_wrap">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={44}
+        color="245, 34, 37"
+        outerAlpha={0.3}
+        innerScale={0.7}
+        outerScale={1.2}
+      />
+      <ScrollToTop />
+      <Routes />
     </div>
   );
 };
