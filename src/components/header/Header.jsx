64d82bc744598d4aef9d16ce4329c4bcc98f4971
BigPump <garrettvk.dev@gmail.com>
2022-01-15 20:17:06 -0800

    logo removed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,11 +29,6 @@ const Header = () => {
       <div className={navbar ? "beny_tm_topbar animate" : "beny_tm_topbar"}>
         <div className="in">
           <div className="topbar_inner">
-            <div className="logo">
-              <Link to="/">
-                <img src="img/logo/dark.png" alt="brand" />
-              </Link>
-            </div>
             <div className="menu">
               <Scrollspy
                 className="anchor_nav"
