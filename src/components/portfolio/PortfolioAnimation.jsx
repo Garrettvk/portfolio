3cd7a7aa2b827ddf45db776361d4309977fff06d
BigPump <garrettvk.dev@gmail.com>
2022-01-16 18:43:46 -0800

    deleting Modal videos

--- src/components/portfolio/PortfolioAnimation.jsx
+++ src/components/portfolio/PortfolioAnimation.jsx
@@ -10,23 +10,7 @@ const Portfolio = () => {
     <SimpleReactLightbox>
       <div className="portfolio_list">
         <ul className="gallery_zoom">
-          <ModalVideo
-            channel="youtube"
-            autoplay
-            isOpen={isOpen}
-            videoId="1gyTUHP6ne8"
-            onClose={() => setOpen(false)}
-          />
-          {/* End Youtube Modal video */}
 
-          <ModalVideo
-            channel="vimeo"
-            autoplay
-            isOpen={isOpen2}
-            videoId="100171151"
-            onClose={() => setOpen2(false)}
-          />
-          {/* End Youtube Modal video */}
 
           <li data-aos="fade-right" data-aos-duration="1200">
             <div className="list_inner video">
@@ -90,75 +74,6 @@ const Portfolio = () => {
 
                 <a href="img/portfolio/1.jpg">
                   <img src="img/portfolio/1.jpg" alt="Dribbble Shoot" />
-                </a>
-              </div>
-            </li>
-            {/* End image popup */}
-
-            <li
-              data-aos="fade-right"
-              data-aos-duration="1200"
-              data-aos-delay="300"
-            >
-              <div className="list_inner">
-                <a
-                  className="title"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://dribbble.com/shots/16530034-Tokyo-Personal-Portfolio-React-Template-RTL"
-                >
-                  <h3>Kelly Smith</h3>
-                  <span>Dribbble Shoot</span>
-                </a>
-
-                <a href="img/portfolio/4.jpg">
-                  <img src="img/portfolio/4.jpg" alt="Dribbble Shoot" />
-                </a>
-              </div>
-            </li>
-            {/* End image popup */}
-
-            <li
-              data-aos="fade-right"
-              data-aos-duration="1200"
-              data-aos-delay="400"
-            >
-              <div className="list_inner">
-                <a
-                  className="title"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://dribbble.com/shots/16529282-Shane-Personal-Portfolio-React-Template"
-                >
-                  <h3>Duru Sweet</h3>
-                  <span>Dribbble Shoot</span>
-                </a>
-
-                <a href="img/portfolio/5.jpg">
-                  <img src="img/portfolio/5.jpg" alt="Dribbble Shoot" />
-                </a>
-              </div>
-            </li>
-            {/* End image popup */}
-
-            <li
-              data-aos="fade-right"
-              data-aos-duration="1200"
-              data-aos-delay="500"
-            >
-              <div className="list_inner">
-                <a
-                  className="title"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.facebook.com/ibthemes"
-                >
-                  <h3>Lisa Kotlins</h3>
-                  <span>Facebook Shoot</span>
-                </a>
-
-                <a href="img/portfolio/6.jpg">
-                  <img src="img/portfolio/6.jpg" alt="Facebook Shoot" />
                 </a>
               </div>
             </li>
