45206683564e16e2825b4812be8b6c9a12969ffc
BigPump <garrettvk.dev@gmail.com>
2022-02-27 13:01:01 -0800

    portfolio shows up on main page now

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Preview from "../views/Preview";
 import HomeLightAnimation from "../views/all-home-version/HomeLightAnimation";
-import HomeTyperCreative from "../views/all-home-version/HomeTyperCreative";
-import HomeParallaxTyper from "../views/all-home-version/HomeParallaxTyper";
-import HomeDarkAnimation from "../views/all-home-version/HomeDarkAnimation";
 import NotFound from "../views/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
@@ -14,11 +10,7 @@ const Routes = () => {
       <Router>
         <ScrollTopBehaviour />
         <Switch>
-          <Route exact path="/" component={Preview} />
-          <Route path="/home-light-animation" component={HomeLightAnimation} />
-          <Route path="/home-dark-animation" component={HomeDarkAnimation} />
-          <Route path="/home-typer-creative" component={HomeTyperCreative} />
-          <Route path="/home-parallax-typer" component={HomeParallaxTyper} />
+          <Route exact path="/" component={HomeLightAnimation} />
           <Route component={NotFound} />
         </Switch>
       </Router>
