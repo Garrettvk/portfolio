2da2ecbd1b8040beec4c1ddc28f19087b9a034ab
Garrett vk <garrettvk.dev@gmail.com>
2024-05-26 21:41:20 -0700

    sample background image and navigation bar changed to white

--- src/components/slider/SliderAnimation.jsx
+++ src/components/slider/SliderAnimation.jsx
@@ -11,8 +11,9 @@ const SliderAnimation = () => {
         <div
           className="image"
           style={{
+            // background image for home page
             backgroundImage: `url(${
-              process.env.PUBLIC_URL + "img/slider/3.jpg"
+              process.env.PUBLIC_URL + "https://c.wallhere.com/photos/d6/ae/1920x950_px_anime_Anime_Girls_Hoshi_Ori_Yume_Mirai_kimono_Narusawa_Rikka_Traditional_Clothing_umbrella-1515277.jpg!d"
             })`,
           }}
         ></div>
